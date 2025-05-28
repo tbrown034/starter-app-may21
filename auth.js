da09223d6b705f47c8f93f2a10190f141f52077d
tbrown034 <tbrown034@gmail.com>
2025-05-27 21:29:11 -0400

    wip, db works in dev, trying in prod

--- auth.js
+++ auth.js
@@ -1,18 +1,25 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import NeonAdapter from "@auth/neon-adapter";
+import { Pool } from "@neondatabase/serverless";
 
 const isProd = process.env.VERCEL_ENV === "production";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: isProd
-        ? process.env.AUTH_GOOGLE_ID
-        : process.env.AUTH_GOOGLE_ID_DEV,
-      clientSecret: isProd
-        ? process.env.AUTH_GOOGLE_SECRET
-        : process.env.AUTH_GOOGLE_SECRET_DEV,
-    }),
-  ],
-  secret: process.env.AUTH_SECRET,
+export const { handlers, signIn, signOut, auth } = NextAuth(() => {
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+  return {
+    adapter: NeonAdapter(pool),
+    providers: [
+      GoogleProvider({
+        clientId: isProd
+          ? process.env.AUTH_GOOGLE_ID
+          : process.env.AUTH_GOOGLE_ID_DEV,
+        clientSecret: isProd
+          ? process.env.AUTH_GOOGLE_SECRET
+          : process.env.AUTH_GOOGLE_SECRET_DEV,
+      }),
+    ],
+    secret: process.env.AUTH_SECRET,
+  };
 });
